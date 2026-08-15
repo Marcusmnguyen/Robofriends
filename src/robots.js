984ea90041729caa3f83ac6bf3e480781592a01d
marcusmnguyen <marcusmnguyen@gmail.com>
2026-08-15 23:10:58 +1000

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Naomi Nguyen',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Jenny Pham',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Marcus Nguyen',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Ong Quoai Quy',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Ba Quoai Thoa',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Katriona Nguyen',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Ba Noi Hong',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
